80c28d9e70d329b85ef3d65ea558c8ee39302320
Pascal Bugnion <pascal@bugnion.org>
2018-12-17 10:47:53 +0000

    Expand the demo slightly

--- examples/app.py
+++ examples/app.py
@@ -3,6 +3,7 @@ from dash import Dash
 
 import dbc_custom_components as dbccc
 import dash_bootstrap_components as dbc
+import dash_html_components as html
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -10,7 +11,13 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container(
     [
-        dbccc.MyBadge(color="secondary")
+        html.H1("Demo app"),
+        dbc.Row(
+            [
+                dbc.Col(dbccc.MyBadge(color="secondary"), width=2),
+                dbc.Col(html.Pre("<- That is a custom badge component"))
+            ]
+        )
     ]
 )
 
